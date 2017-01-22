32b8d74a690a89edaf3b60e4edafed654f1a3668
Brandon Manke <brandonmanke@users.noreply.github.com>
2017-01-22 15:11:04 -0600

    Added modernizr and some older browser support Added some older browser support also added modernizr Right now all modernizr does is check if the browser supports svgs and switches them to pngs if they do not support it. Also cleaned a couple minor things up.

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,1 +1,25 @@
-console.log('load');
+/* script.js
+ * modernizr checks mainly
+ */
+window.onload = function() {
+
+	function svgToPNG(element) {
+		element.src = 'assets/images/' + element.id + '.png';
+		//element.width = '60';
+		//element.height = '60';
+	}
+
+	if (Modernizr.svg) {
+		console.log('svg');
+	} else {
+		var mail = document.getElementById('mail');
+		var github = document.getElementById('github');
+		var linkedin = document.getElementById('linkedin');
+		var stackoverflow = document.getElementById('stackoverflow');
+
+		svgToPNG(mail);
+		svgToPNG(github);
+		svgToPNG(linkedin);
+		svgToPNG(stackoverflow);
+	}
+}
