ce85b107a16f0b3dd606cb67c2d199407516e6f3
Brandon Manke <me@brandonmanke.com>
2017-04-10 15:41:32 -0500

    Formatted file spacing

--- assets/js/script.js
+++ assets/js/script.js
@@ -20,16 +20,16 @@ window.onload = function() {
     });
 
     if (Modernizr.svg) {
-    	console.log('svg');
+        console.log('svg');
     } else {
-    	var mail = document.getElementById('mail');
-    	var github = document.getElementById('github');
-    	var linkedin = document.getElementById('linkedin');
-    	var stackoverflow = document.getElementById('stackoverflow');
+        var mail = document.getElementById('mail');
+        var github = document.getElementById('github');
+        var linkedin = document.getElementById('linkedin');
+        var stackoverflow = document.getElementById('stackoverflow');
 
-    	mail.src = 'assets/images/' + mail + '.png';
-    	github.src = 'assets/images/' + github + '.png';
-    	linkedin.src = 'assets/images/' + linkedin + '.png';
-    	stackoverflow.src = 'assets/images/' + stackoverflow + '.png';
+        mail.src = 'assets/images/' + mail + '.png';
+        github.src = 'assets/images/' + github + '.png';
+        linkedin.src = 'assets/images/' + linkedin + '.png';
+        stackoverflow.src = 'assets/images/' + stackoverflow + '.png';
     }
 }
