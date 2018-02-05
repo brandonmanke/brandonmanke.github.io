c2786b8bc9625d072f10082296117c9a34a09806
Brandon Manke <manke.brandon@gmail.com>
2018-02-05 00:22:11 -0600

    Added about me section 90% dont with about me section although there is some spacing issues with list elements. Last major task is the projects section. After that is complete will work on polishing the entire page.

--- script/script.js
+++ script/script.js
@@ -1,6 +1,11 @@
 /**
  * Idea: make each gradient appear one at a time 
  * or maybe make top then bottom ones appear etc.
+ *
+ * TODO:
+ * Add noscript tags
+ * Also try to add as much support for older browsers
+ * Noticed a weird bug with the skewed gradients on safari
  */
 ;(function() {
     window.onload = function() {
