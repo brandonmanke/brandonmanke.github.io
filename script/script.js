d0ec275e583b51136bda29abb86332efe66d8e06
Brandon Manke <manke.brandon@gmail.com>
2018-02-06 00:21:42 -0600

    Added initial project section Updated README.md Added projects section with proper links Added links to social media buttons

--- script/script.js
+++ script/script.js
@@ -2,9 +2,15 @@
  * Idea: make each gradient appear one at a time 
  * or maybe make top then bottom ones appear etc.
  *
+ * Another really interesting but hard idea:
+ * Somehow simulate a shooting star in the background
+ * that would ocassionally appear in different areas of the screen.
+ * I believe I would need to use canvas to implement something like this.
+ *
  * TODO:
  * Add noscript tags
  * Also try to add as much support for older browsers
+ * (Check out mondernizr)
  * Noticed a weird bug with the skewed gradients on safari
  */
 ;(function() {
