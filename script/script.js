8fc12392aeadd7512de17367dd1eb30f51697040
Brandon Manke <manke.brandon@gmail.com>
2019-06-27 22:14:42 -0500

    add: website redesign

--- script/script.js
+++ script/script.js
@@ -1,18 +1,4 @@
-/**
- * Idea: make each gradient appear one at a time 
- * or maybe make top then bottom ones appear etc.
- *
- * Another really interesting but hard idea:
- * Somehow simulate a shooting star in the background
- * that would ocassionally appear in different areas of the screen.
- * I believe I would need to use canvas to implement something like this.
- *
- * TODO:
- * Add noscript tags
- * Also try to add as much support for older browsers
- * (Check out mondernizr)
- * Noticed a weird bug with the skewed gradients on safari
- */
+//TODO: add noscript tags
 ;(function() {
     window.onload = function() {
         setTimeout(function() {
@@ -20,10 +6,10 @@
         }, 500)
         setTimeout(function() {
             document.querySelector('.title').className += ' show'
-        }, 1000)
+        }, 900)
         setTimeout(function() {
             document.querySelector('.about').className += ' show'
             document.querySelector('.projects').className += ' show'
-        }, 1000)
+        }, 900)
     }
 })(this)
